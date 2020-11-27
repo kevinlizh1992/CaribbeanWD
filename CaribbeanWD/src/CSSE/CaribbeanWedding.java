e30b5f001fe5a42c908aa19b9919e1ad727989fc
kevinlizh1992 <43556063+kevinlizh1992@users.noreply.github.com>
2020-11-26 19:35:33 -0500

    Update CaribbeanWedding.java

--- CaribbeanWD/src/CSSE/CaribbeanWedding.java
+++ CaribbeanWD/src/CSSE/CaribbeanWedding.java
@@ -1,7 +1,7 @@
 package CSSE;
 /**
  * 
- * @author rmorales
+ * @author kzhli
  *
  */
 public class CaribbeanWedding {
@@ -12,8 +12,17 @@ public class CaribbeanWedding {
 		 wp.cost());
 		
 		//TODO: Instantiate a new wp2 of a Soul package with DJ, Photo, and video recording
+		WeddingPackage wp2 = new Soul();
+		wp2 = new DjMusic(wp2);
+		wp2 = new PhotoService(wp2);
+		wp2 = new VideoRecording(wp2);
+		System.out.println(wp.getDescription() + " cost $" + wp2.cost());
 		
 		//TODO: Instantiate a new wp3 of a Deluxe package with private dinner on the beach, and 20 guests
+		WeddingPackage wp3 = new Deluxe();
+		wp2 = new PrivateDinnerBeach(wp2, 20);
+		System.out.println(wp.getDescription() + " cost $" + wp2.cost() + );
+	
 		
 		
 	}
